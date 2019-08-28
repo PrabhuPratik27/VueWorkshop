434cfe89a9b0a4d9b99b2ef161d2b70b8286020b
Pratik <prabhupratik27@gmail.com>
2019-08-28 16:52:52 +0530

    Initial Commit

---
+++ precache-manifest.93d0a00727ebc2320ce78686b6a94dfa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/VueWorkshop/robots.txt"
+  },
+  {
+    "revision": "15f5877dc12f2ffdba86",
+    "url": "/VueWorkshop/js/chunk-vendors.bee82365.js"
+  },
+  {
+    "revision": "c8a0cf6854b3970c00f4",
+    "url": "/VueWorkshop/js/app.48405094.js"
+  },
+  {
+    "revision": "782621e4e3046fa46892bca6770467c5",
+    "url": "/VueWorkshop/index.html"
+  },
+  {
+    "revision": "f736edc020068094021cd05413307d11",
+    "url": "/VueWorkshop/img/section.f736edc0.jpg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/VueWorkshop/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "24909e5f74d91328d1d0c13c66069699",
+    "url": "/VueWorkshop/img/coclogowithname.24909e5f.png"
+  },
+  {
+    "revision": "417ff2aabd8cd374daee0fcc836c9864",
+    "url": "/VueWorkshop/img/coc_logo.417ff2aa.png"
+  },
+  {
+    "revision": "15f5877dc12f2ffdba86",
+    "url": "/VueWorkshop/css/chunk-vendors.a53438d5.css"
+  },
+  {
+    "revision": "c8a0cf6854b3970c00f4",
+    "url": "/VueWorkshop/css/app.d1e9b7ea.css"
+  }
+];
